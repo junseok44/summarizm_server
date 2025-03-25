d4cd6baa98c27e96168c555398584e3bf009a67c
newtonjjang <newtonjjang@naver.com>
2025-03-25 17:56:50 +0900

    feat: 파일시스템 output-channel 클래스 생성

---
+++ src/output-channel/channels/file-system.channel.ts
@@ -0,0 +1,62 @@
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  IOutputChannel,
+  IStorageData,
+} from '../interfaces/output-channel.interface';
+import { ConfigService } from '@nestjs/config';
+
+import * as path from 'path';
+import * as fs from 'fs/promises';
+
+@Injectable()
+export class FileSystemChannel implements IOutputChannel {
+  private readonly logger: Logger = new Logger('File System Channel'); // 로깅
+
+  constructor(
+    private readonly configService: ConfigService, // 설정 관리
+  ) {}
+
+  getName(): string {
+    return 'file';
+  }
+  async getStatus(): Promise<boolean> {
+    const storagePath = path.join(
+      process.cwd(),
+      this.configService.get('FILE_STORAGE_PATH'),
+    );
+
+    try {
+      await fs.access(storagePath);
+      return true;
+    } catch {
+      try {
+        await fs.mkdir(storagePath, { recursive: true });
+        return true;
+      } catch (error) {
+        this.logger.error(`저장소 생성 실패: ${error.message}`);
+        return false;
+      }
+    }
+  }
+
+  async send(data: IStorageData): Promise<void> {
+    const filePath = path.join(
+      process.cwd(),
+      this.configService.get('FILE_STORAGE_PATH'),
+      data.fileName,
+    );
+
+    try {
+      const content =
+        typeof data.content === 'string'
+          ? data.content
+          : JSON.stringify(data.content, null, 2);
+
+      await fs.writeFile(filePath, content);
+      this.logger.log(`File saved: ${data.fileName}`);
+    } catch (error) {
+      this.logger.error(`Failed to save file: ${error.message}`);
+      throw new Error('파일 저장 실패');
+    }
+  }
+}
