2f6ebf6ad04abb1b649aa798e45afffe9ab6298a
newtonjjang <newtonjjang@naver.com>
2025-03-25 17:47:36 +0900

    feat: 앱 보일러플레이트 초기화

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SummaryModule } from './summary/summary.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './common/filter/all-exceptions.filter';
+
+@Module({
+  imports: [
+    SummaryModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        YOUTUBE_API_KEY: Joi.string().required(),
+        CLAUDE_API_KEY: Joi.string().required(),
+        FILE_STORAGE_PATH: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
+})
+export class AppModule {}
